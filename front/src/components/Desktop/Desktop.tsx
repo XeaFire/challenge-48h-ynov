17d81258ce0ff3ccee97c86e06dbc612ae326fa7
DTristan <tristan.diard@ynov.com>
2026-03-30 13:08:39 +0200

    feat:Add File Explorer

--- front/src/components/Desktop/Desktop.tsx
+++ front/src/components/Desktop/Desktop.tsx
@@ -1,7 +1,7 @@
 import type { ReactNode } from 'react';
 import type { WindowType } from '../../types';
 import { DesktopIcon } from './DesktopIcon';
-import { ComputerIcon, NotepadIcon, RecycleBinIcon, InternetExplorerIcon, CalculatorIcon, PaintIcon } from '../../icons';
+import { ComputerIcon, NotepadIcon, RecycleBinIcon, InternetExplorerIcon, CalculatorIcon, PaintIcon, ExplorerIcon } from '../../icons';
 
 interface DesktopProps {
   onOpenWindow: (type: WindowType) => void;
@@ -53,15 +53,22 @@ const DESKTOP_ICONS: DesktopIconConfig[] = [
     action: { type: 'openWindow', windowType: 'calculator' },
   },
   {
-    id: 'paint',
+    id: 'explorer',
     x: 10, y: 370,
+    icon: <ExplorerIcon />,
+    label: 'Explorateur',
+    action: { type: 'openWindow', windowType: 'explorer' },
+  },
+  {
+    id: 'paint',
+    x: 10, y: 460,
     icon: <PaintIcon />,
     label: 'Paint',
     action: { type: 'openWindow', windowType: 'paint' },
   },
   {
     id: 'ie',
-    x: 10, y: 460,
+    x: 10, y: 550,
     icon: <InternetExplorerIcon />,
     label: 'Internet Explorer',
     action: { type: 'triggerBSOD' },
